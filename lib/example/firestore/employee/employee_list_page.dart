a694ce5830bb5ff898167d235e4326b9d312bcdb
TGA88 <supawut.thomas@gmail.com>
2020-07-23 18:39:57 +0700

    add example for firestore

---
+++ lib/example/firestore/employee/employee_list_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EmployeeListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
